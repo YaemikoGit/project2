dc0e119ed67827a9d8822eaf5d1887fc3fb11ad7
YaemikoGit <102906443+YaemikoGit@users.noreply.github.com>
2024-02-15 23:16:34 +0800

    added sceneManager class

---
+++ desktop/src/com/mygdx/game/Scene.java
@@ -0,0 +1,27 @@
+package com.mygdx.game;
+
+public class Scene {
+	private String sceneName;
+	private String bgImage;
+	
+	
+	public String getSceneName() {
+		return sceneName;
+	}
+	
+	public void setSceneName(String s) {
+		sceneName = s;
+	}
+	
+	public String getBgImage() {
+		return bgImage;
+	}
+	
+	public void setBgImage(String b) {
+		bgImage = b;
+	}
+	
+	public void display() {
+		
+	}
+}
---
+++ desktop/src/com/mygdx/game/SceneManager.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class SceneManager {
+	
+	private List<Scene> sceneList;
+	private Scene currentScene;
+	
+	
+	//get current scene
+	
+	//load screen
+	
+	//unload scene
+}
